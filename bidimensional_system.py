a82e85c1f0071643b2ad885071e876136071f087
Xus72 <jexusth@hotmail.com>
2022-12-06 03:53:19 -0500

    fix equation system and functions

--- bidimensional_system.py
+++ bidimensional_system.py
@@ -1,106 +1,51 @@
 #!/usr/bin/env sage -python
 from sage.all import *
 
-'''def define_system():
-    xn,yn = var('xn,yn')
-    x = var('x')
-    a,b = var('a,b')
-    f = b*x+2*(1-b)**(x**2/1+x**2)
-    fn = vector([a * yn + f(x=xn), - xn + f(x=xn+1)])
-    return xn, yn, fn'''
+def define_system():
+    x, y = var('x,y')
+    a, b = var('a,b')
+    f = b * x + 2 * (1 - b) ** (x ** 2 / 1 + x ** 2)
+    fn = vector([a * y + f(x=x), - x + f(x=x)])
+    return x, y, fn, a, b
 
-def fixed_points(fn,a,b,xn,yn):
-    sol = solve([fn(a=a,b=b)[0]==xn, fn(a=a,b=b)[1]==yn], xn,yn)
+def fixed_points(fn,x,y):
+    sol = solve([fn[0]==x, fn[1]==y], x,y)
     return sol
 
-def jacobian_matrix(fn,a,b,xn,yn,fix_p):
-    fx = fn(a=a,b=b).diff(xn)
-    fy = fn(a=a,b=b).diff(yn)
+def jacobian_matrix(f,x,y,fix_p):
+    fx = f.diff(x)
+    fy = f.diff(y)
     Df = matrix([fx, fy]).transpose()
-    show(Df)
+    Df = Df(x=fix_p[0],y=fix_p[1])
     return Df
 
 def eigenvalues(fix_p,df):
+    eigenval_abs = list()
     for i in range(len(fix_p)):
-        show(df(xn=fix_p[i][0],yn=fix_p[i][1]))
-        eigenvalue = [df(xn=fix_p[i][0].right(),yn=fix_p[i][1].right()).eigenvalues()]
-        show(eigenvalue)
+        d = df(x=fix_p[i][0],y=fix_p[i][1])
+        eigenvalue = [d.eigenvalues()]
         for j in range(len(eigenvalue)):
-            lambda_1 = eigenvalue[i][0]
-            lambda_2 = eigenvalue[i][1]
-            show(lambda_1)
-            show(lambda_2)
-            show(lambda_1.abs().n())
-            show(lambda_2.abs().n())
-            '''if lambda_1.abs() < 1 & lambda_2.abs() < 1:
-                show("The fixed point " + sol[i] + "is a sink point")
-            if lambda_1.abs().n() > 1.0 & lambda_2.abs().n > 1.0:
-                show("The fixed point " + sol[i] + "is a source point")
-            if (lambda_1.abs().n() < 1 & lambda_2.abs().n() > 1) | (lambda_1.abs().n() > 1 & lambda_2.abs().n() < 1):
-                show("The fixed point " + sol[i] + "is a saddle point")
-            if lambda_1.abs().n() == 1 | lambda_2.abs().n() == 1:
-                show("The fixed point " + sol[i] + "is a non-hyperbolic point")'''
-    return None
+            lambda_1 = eigenvalue[i][0].abs().n()
+            lambda_2 = eigenvalue[i][1].abs().n()
+            eigenval_abs.append([lambda_1,lambda_2])
+    return eigenval_abs
 
-def periodic_points(fn,a,b,xn,yn):
-    sol_2_per = solve([fn(xn=fn[0],yn=fn[1],a=a,b=b)[0]==xn, fn(xn=fn[0],yn=fn[1],a=a,b=b)[1]==yn], xn,yn)
+def periodic_points(fn,x,y):
+    sol_2_per = solve([fn(x=fn[0],y=fn[1])[0]==x, fn(x=fn[0],y=fn[1])[1]==y], x,y)
     return sol_2_per
 
-def attractors(fn,a,b,xn,yn,x0,n,m):
-    points = list()
+def jacobian_matrix_2(f,x,y,fix_p):
+    Df = jacobian_matrix(f,x,y,fix_p)
+    Df2 = Df(x=fix_p[0], y=fix_p[1])*Df(x=fix_p[1],y=fix_p[0])
+    return Df2
+def eigenvectors(df,fix_p):
+    eigenvector = list()
+    for i in range(len(fix_p)):
+        eigenvector_left = df(x=fix_p[i][0],y=fix_p[i][1]).eigenvectors_left()
+        eigenvector_right = df(x=fix_p[i][0],y=fix_p[i][1]).eigenvectors_left()
+        eigenvector.append(eigenvector_left,eigenvector_right)
+    return eigenvector
 
-    for i in range(n-1):
-        x0 = fn(a=a,b=b,xn=x0[0],yn=x0[1]).n()
-    for i in range(n,m+1):
-        x0 = fn(a=a,b=b,xn=x0[0],yn=x0[1]).n()
-        points = [x0]
-
-    return plot(point(points))
-
-def orbits(fn,a,b,xn,yn,x0,n):
-    g = Graphics()
-    points = [x0]
-    g += point(x0,rgcolor='red')
-
-    for i in range(n-1):
-        x0 = fn(a=a,b=b,xn=x0[0],yn=x0[1])
-        points += [x0]
-        g += point(x0, rgcolor='red')
-    for i in range(n-1):
-        g += line([points[i], points[i+1]], rgcolor='blue')
-
-    return g
-
-def final_orbits(fn,a,b,xn,yn,x0,n,m):
-    g = Graphics()
-    points = list()
-    g += point(x0, rgcolor='red')
-
-    for i in range(n-1):
-        x0 = fn(a=a,b=b,xn=x0[0],yn=x0[1])
-    for i in range(n,m+1):
-        x0 = fn(a=a,b=b,xn=x0[0],yn=x0[1])
-        points += [x0]
-        g += point(x0, rgcolor='red')
-    for i in range(m-n):
-        g += line([points[i], points[i+1]], rgcolor='blue')
-
-    return g
-
-if __name__ == '__main__':
-    xn, yn = var('xn,yn')
-    x = var('x')
-    a,b = var('a,b')
-    f = b * x + 2 * (1 - b) ** (x ** 2 / 1 + x ** 2)
-    fn = vector([a * yn + f(x=xn), - xn + f(x=xn + 1)])
-    a = 0
-    b = 0
-    sol = fixed_points(fn,a,b,xn,yn)
-    df = jacobian_matrix(fn,a,b,xn,yn,sol)
-    eigenvalues(sol,df)
-    sol_f2 = periodic_points(fn,a,b,xn,yn)
-    show(sol_f2)
-    df2 = jacobian_matrix(fn,a,b,xn,yn,sol_f2)
-    show(df2)
-    eigenvalues(sol_f2,df2)
-
+'''def varieties(fn,a,b,xn,yn,fix_p,df):
+    if (eigenvalues(fix_p,df)[0] < 1 & eigenvalues(fix_p,df)[1]) > 1 | (eigenvalues(fix_p,df)[0] > 1 & (eigenvalues(fix_p,df))[1]):
+        eigenvector = eigenvectors(df,fix_p)'''
